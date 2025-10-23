d186046417b95b8f15deab7f685cd9eb8f78ae76
son <s0nprem0@proton.me>
2025-10-24 00:40:52 +0800

    feat(home): update Home layout and CTA
    Use .web-container/.container for consistent layout, add descriptive subtitle and CTA links for lab navigation.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,20 @@ import react from 'react'
 
 function Home() {
 	return (
-		<div className="container">
+		<div className="web-container">
 			<div className="home-wrapper">
 				<div className="home-card">
-					<div>
-						<h2>Oops… You’ve Landed in the Lobby 🚪</h2>
-						<p>Card content goes here.</p>
+					<div className='container'>
+						<h1>Welcome🚪</h1>
+						<p className='subtitle'>This page is a purposively for Laboratory Activity #4 - Web Development through React – Routing and Navigation</p>
+					</div>
+					<div className='container'>
+						<p className='subtitle'>This is just a decoy. Use the links below to simulate where to go where the action is.<br></br>This button below is without a react router applied because it reloads the whole page </p>
+						<div className='cta'>
+							<a className='btn nav-link' href='/'>Home</a>
+							<a className='btn nav-link' href='/login'>Login</a>
+							<a className='btn nav-link' href='lab3'>Lab Repo</a>
+						</div>
 					</div>
 				</div>
 			</div>
